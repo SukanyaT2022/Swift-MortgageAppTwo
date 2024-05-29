bae788f10072c258c61c81cba3bb27001c86eb71
TiparpronS <96738451+SukanyaT2022@users.noreply.github.com>
2024-05-29 11:59:50 -0400

    first

---
+++ MortgagePractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MortgagePractice
+//
+//  Created by Tiparpron Sukanya on 5/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
